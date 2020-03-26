c7ab3873dbe41455423351e3f7f094a0b914d3e8
Surya <suryasubramaniank@gmail.com>
2020-03-26 14:41:36 +0530

    Commented all except BU

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -7,7 +7,7 @@ Sfdc.canvas(function() {
   // Save the token
   Sfdc.canvas.oauth.token(window.signedRequestJson.oauthToken);
   Sfdc.canvas.byId('username').innerHTML = window.signedRequestJson.context.user.fullName;
-  Sfdc.canvas.byId('bu').innerHTML = window.signedRequestJson.context.environment.parameters.recordId;
+  Sfdc.canvas.byId('bu').innerHTML = window.signedRequestJson.context.environment.parameters.bucode;
   Sfdc.canvas.byId('recId').innerHTML = window.signedRequestJson.recordId;
   Sfdc.canvas.byId('environment').innerHTML = window.signedRequestJson.context.environment;
   Sfdc.canvas.byId('contextDetails').innerHTML = window.signedRequestJson.context;
