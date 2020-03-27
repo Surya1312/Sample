ce5f23d395a98594403151595e2b70c1369ba78f
Surya <suryasubramaniank@gmail.com>
2020-03-27 12:10:13 +0530

    reverting to orginal

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -9,9 +9,9 @@ Sfdc.canvas(function() {
   Sfdc.canvas.oauth.token(window.signedRequestJson.oauthToken);
   Sfdc.canvas.byId('username').innerHTML = window.signedRequestJson.context.user.fullName;
   Sfdc.canvas.byId('bu').innerHTML = window.signedRequestJson.context.environment.parameters.bucode;
-  Sfdc.canvas.byId('oppNo').innerHTML = window.signedRequestJson.context.environment.parameters.oppnumber;
+  /*Sfdc.canvas.byId('oppNo').innerHTML = window.signedRequestJson.context.environment.parameters.oppnumber;
   
-  /*Sfdc.canvas.byId('recId').innerHTML = window.signedRequestJson.recordId;
+  Sfdc.canvas.byId('recId').innerHTML = window.signedRequestJson.recordId;
   Sfdc.canvas.byId('environment').innerHTML = window.signedRequestJson.context.environment;
   Sfdc.canvas.byId('contextDetails').innerHTML = window.signedRequestJson.context;
   Sfdc.canvas.byId('instanceId').innerHTML = window.signedRequestJson.client.instanceId;*/
