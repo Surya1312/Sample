bcf063c15777aa095c1e8192cb2f64e3d60fa84e
Surya <suryasubramaniank@gmail.com>
2020-03-27 09:07:44 +0530

    Small Change in js file

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,20 +3,6 @@ if (self === top) {
   alert("This canvas app must be included within an iframe");
 }
 
-Sfdc.canvas(function() {
-  // Save the token
-  Sfdc.canvas.oauth.token(window.signedRequestJson.oauthToken);
-  Sfdc.canvas.byId('username').innerHTML = window.signedRequestJson.context.user.fullName;
-  
-  Sfdc.canvas.byId('bu').innerHTML = window.signedRequestJson.context.environment.parameters.bucode;
-  Sfdc.canvas.byId('oppNo').innerHTML = window.signedRequestJson.context.environment.parameters.oppnumber;
-  
-  Sfdc.canvas.byId('recId').innerHTML = window.signedRequestJson.recordId;
-  Sfdc.canvas.byId('environment').innerHTML = window.signedRequestJson.context.environment;
-  Sfdc.canvas.byId('contextDetails').innerHTML = window.signedRequestJson.context;
-  Sfdc.canvas.byId('instanceId').innerHTML = window.signedRequestJson.client.instanceId;
-});
-
 function createDynamicURL() {
   //The variable to be returned
   var URL;
@@ -34,4 +20,20 @@ function createDynamicURL() {
 
   return URL;
 }
+
+
+Sfdc.canvas(function() {
+  // Save the token
+  Sfdc.canvas.oauth.token(window.signedRequestJson.oauthToken);
+  Sfdc.canvas.byId('username').innerHTML = window.signedRequestJson.context.user.fullName;
+  
+  Sfdc.canvas.byId('bu').innerHTML = window.signedRequestJson.context.environment.parameters.bucode;
+  Sfdc.canvas.byId('oppNo').innerHTML = window.signedRequestJson.context.environment.parameters.oppnumber;
+  
+  Sfdc.canvas.byId('recId').innerHTML = window.signedRequestJson.recordId;
+  Sfdc.canvas.byId('environment').innerHTML = window.signedRequestJson.context.environment;
+  Sfdc.canvas.byId('contextDetails').innerHTML = window.signedRequestJson.context;
+  Sfdc.canvas.byId('instanceId').innerHTML = window.signedRequestJson.client.instanceId;
+});
+
 
