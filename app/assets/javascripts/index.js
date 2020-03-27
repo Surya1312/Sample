11e9bdba19e7728a8a900f757efbe96574c06024
Surya <suryasubramaniank@gmail.com>
2020-03-27 08:45:04 +0530

    Moved the js code from html to js file for URL

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -17,4 +17,21 @@ Sfdc.canvas(function() {
   Sfdc.canvas.byId('instanceId').innerHTML = window.signedRequestJson.client.instanceId;
 });
 
+function createDynamicURL() {
+  //The variable to be returned
+  var URL;
+
+  //The variables containing the respective IDs
+  var buCode = window.signedRequestJson.context.environment.parameters.bucode;
+  var OpportunityNumber = window.signedRequestJson.context.environment.parameters.oppnumber;
+
+      //Forming the variable to return    
+      URL+="https://ssrs-dev.usgboral.com/ReportServer/Pages/ReportViewer.aspx?/OpportunitySQLDW&BU=";
+  URL += buCode;
+  URL += "&OpportunityNumber=";
+  URL += OpportunityNumber;
+  URL += "&rs:Command=Render";
+
+  return URL;
+}
 
